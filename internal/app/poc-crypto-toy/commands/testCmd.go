38d9ebbf15c116a998017dcfbef19f43582821ec
Nicholas Bering <bering.nicholas@gmail.com>
2019-01-02 02:01:38 -0500

    Make the Go linting tools happy with some comments.

--- internal/app/poc-crypto-toy/commands/testCmd.go
+++ internal/app/poc-crypto-toy/commands/testCmd.go
@@ -8,22 +8,31 @@ import (
 	"github.com/google/subcommands"
 )
 
+// TestCmd is a subcommand implementation that just demonstrates subcommand usage.
 type TestCmd struct {
 	foo bool
 }
 
-func (*TestCmd) Name() string     { return "test" }
+// Name returns the name of the TestCmd subcommand used in the CLI implementation.
+func (*TestCmd) Name() string { return "test" }
+
+// Synopsis returns a short description of TestCmd used in the CLI implementation.
 func (*TestCmd) Synopsis() string { return "Testing out the Google subcommands module." }
+
+// Usage returns a brief usage description of TestCmd for the help text
+// in the CLI implementation.
 func (*TestCmd) Usage() string {
 	return `test [-foo]:
 	Doesn't really do anything, yet.
 `
 }
 
+// SetFlags sets up the command line flags for the TestCmd subcommand.
 func (t *TestCmd) SetFlags(f *flag.FlagSet) {
 	f.BoolVar(&t.foo, "foo", false, "foo the bar in the biz baz for the buzz")
 }
 
+// Execute provides the actual command implementation of TestCmd.
 func (t *TestCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	fmt.Printf("Foo!")
 	fmt.Println()
